12968a68a43b24959a7cb572800906d8da60bda3
michalovsky <michalovskyyy@gmail.com>
2020-07-26 19:18:15 +0200

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+USE join-us-db;
+
+CREATE TABLE users
+(
+    email VARCHAR(255) PRIMARY KEY,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+INSERT INTO users(email) VALUES ("Katie34@gmail.com"), ("Rrqwq@gmail.com");
